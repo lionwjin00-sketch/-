be98d743d9bc8e1185860f283ece6aa5fa778054
lionwjin00-sketch <lionwjin00@gmail.com>
2026-09-13 23:59:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const WhatTodo = document.querySelector("#WhatTodo");
 const addbutton = document.querySelector("#addbutton");
 const todoList = document.querySelector("#todoList");
 
+let todos = [
+    {
+        id: 1,
+        text: "JavaScript 공부",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "운동하기",
+        completed: true
+    }
+];
+
+function showTodos() {
+    for (let i = 0; i < todos.length; i++) {
+       const li = document.createElement("li");
+    li.textContent = todos[i].text;
+    todoList.appendChild(li);
+    }
+}
+showTodos();
