437f0c23461a8138247d12edecfecbf3e83a36e0
lionwjin00-sketch <lionwjin00@gmail.com>
2026-09-14 00:08:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const WhatTodo = document.querySelector("#WhatTodo");
 const addbutton = document.querySelector("#addbutton");
 const todoList = document.querySelector("#todoList");
 
-alert("fddddddfdas")
+alert("fddddddfdas");
 console.log("asdf");
 let todos = [
     {
