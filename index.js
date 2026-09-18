148bfa556843da0697311affa013f955a9d5c27c
lionwjin00-sketch <lionwjin00@gmail.com>
2026-09-19 00:39:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ let todos = [
 ];
 
 function showTodos() {
+    todoList.innerHTML = "";
+    
     for (let i = 0; i < todos.length; i++) {
        const li = document.createElement("li");
     li.textContent = todos[i].text;
@@ -23,3 +25,16 @@ function showTodos() {
     }
 }
 showTodos();
+
+addbutton.addEventListener("click", function () {
+    const text = WhatToDo.value;
+
+    todos.push({
+        id: todos.length + 1,
+        text: text,
+        completed: false
+    });
+
+    showTodos();
+    WhatToDo.value = "";
+});
